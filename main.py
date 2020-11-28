3180cad09a92ab43eef24d3e47bd8073db7a293e
Josef <josef733@hotmail.com>
2020-11-28 01:36:38 +0100

    Initialization Step Added
    Code for initialization added. Complete with user selection

--- main.py
+++ main.py
@@ -1,12 +1,39 @@
+import sys
+
 #Init
-input1 = ""
-input2 = 0
-input3 = ""
+a = 0
 
-while (1 > 2):
-	pass
+while (a = 0):
+	
+	url = input("Enter the URL for crawling: ")
+	level = int(input("Enter the depth of crawling (help in readme): "))
+	
+	regexps = []
+	b = 0
 
-print()
+	add = input("Would you like to add any regular expressions? ")
+
+	if (add = "yes"):
+		while (b = 0):
+			exp = input("Enter a regular expression: ")
+			regexps.append(exp)
+			confirmexp = input("Would you like to add another expression? ")
+			if (confirmexp = "yes"):
+				b = 0
+			else:
+				b = 1
+
+	print("Selected Parameters:\n")
+	print("URL: " + url)
+	print("Crawling Depth: " + str(level))
+	print("Regular Expressions: "+ regexps)
+	confirm = input("Proceed with website crawling (yes/no)? ")
+
+	if (confirm = "yes"):
+		a = 1
+
+	else:
+		a = 0
 
 #Main
 
