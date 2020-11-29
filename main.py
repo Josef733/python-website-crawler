34247ae679a4c6aa391950dd469c37fd671fd82f
Josef <josef733@hotmail.com>
2020-11-29 22:28:26 +0100

    misc files + init cleanup
    miscellanous folder added. main file cleaned up more

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
+import re
 import sys
+import scrapy
 
 #Init
 a = 0
 
-while (a = 0):
+while (a == 0):
 	
 	url = input("Enter the URL for crawling: ")
 	level = int(input("Enter the depth of crawling (help in readme): "))
@@ -13,12 +15,12 @@ while (a = 0):
 
 	add = input("Would you like to add any regular expressions? ")
 
-	if (add = "yes"):
-		while (b = 0):
+	if (add == "yes"):
+		while (b == 0):
 			exp = input("Enter a regular expression: ")
 			regexps.append(exp)
 			confirmexp = input("Would you like to add another expression? ")
-			if (confirmexp = "yes"):
+			if (confirmexp == "yes"):
 				b = 0
 			else:
 				b = 1
@@ -26,16 +28,25 @@ while (a = 0):
 	print("Selected Parameters:\n")
 	print("URL: " + url)
 	print("Crawling Depth: " + str(level))
-	print("Regular Expressions: "+ regexps)
+	print("Regular Expressions: "+ str(regexps))
 	confirm = input("Proceed with website crawling (yes/no)? ")
 
-	if (confirm = "yes"):
+	if (confirm == "yes"):
 		a = 1
 
+	elif (confirm == "no"):
+		restart = input("Would you like to restart? ")
+		if (restart == "yes"):
+			a = 0
+		else:
+			sys.exit()
 	else:
-		a = 0
+		sys.exit()
 
 #Main
+print("Starting...")
 
 
 #Output/Downloads
+
+print("Complete!")
